57ea56774d6b8763583a2c82a98f5270d2645423
Lisha Liang <lishaliang1999@gmail.com>
2022-06-10 21:46:53 +0000

    client

--- client.js
+++ client.js
@@ -1,11 +1,13 @@
 const net = require("net");
+const { IP, PORT } = require("./constant");
+
 
 const connect = function () {
   console.log("Connecting ...");
 
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
