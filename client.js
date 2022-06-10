70ced0e95b4f273d7e1c65a6e99076a4d6ac2fbf
Lisha Liang <lishaliang1999@gmail.com>
2022-06-10 19:03:23 +0000

    client

--- client.js
+++ client.js
@@ -9,29 +9,29 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
   
-  conn.on("connect", () => {
-    conn.write("Name: LLS")//sending message to server
-  });
+  // conn.on("connect", () => {
+  //   conn.write("Name: LLS")//sending message to server
+  // });
 
 
-  conn.on("connect", () => {
-    setTimeout(() => {
-      conn.write("Move: up")//sending message to server
-    }, 1000)
-  });  
+  // conn.on("connect", () => {
+  //   setTimeout(() => {
+  //     conn.write("Move: up")//sending message to server
+  //   }, 1000)
+  // });  
 
-  conn.on("connect", () => {
-    setTimeout(() => {
-      conn.write("Move: left")//sending message to server
-    }, 1300)
-  });  
+  // conn.on("connect", () => {
+  //   setTimeout(() => {
+  //     conn.write("Move: left")//sending message to server
+  //   }, 1300)
+  // });  
 
 
-  conn.on("data", (data) => {//the message from server
-    console.log(data) //handling incoming data
-  })
+  // conn.on("data", (data) => {//the message from server
+  //   console.log(data) //handling incoming data
+  // })
 
-  console.log('you ded cuz you idled')
+  // console.log('you ded cuz you idled')
   return conn;
 
 
@@ -39,9 +39,9 @@ const connect = function () {
 };
 
 console.log("Connecting ...");
-connect();
+module.exports = connect
 
-console.log(myObject); // => { A: 42, B: 43, C: 44 }
-console.log(myObject.A); // => 42
-console.log(myObject.B); // => 43
-console.log(myObject.C); // => 44
+// console.log(myObject); // => { A: 42, B: 43, C: 44 }
+// console.log(myObject.A); // => 42
+// console.log(myObject.B); // => 43
+// console.log(myObject.C); // => 44
