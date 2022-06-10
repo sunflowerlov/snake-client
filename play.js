2efdff7207e6aa0126d7fcb7af665167f9f34a2e
Lisha Liang <lishaliang1999@gmail.com>
2022-06-10 16:21:43 +0000

    server

--- play.js
+++ play.js
@@ -1,19 +1,20 @@
 const net = require("net");
 
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243",
+    host: "10.0.2.15",
     port: 50541,
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
   
   conn.on("connect", () => {
-    conn.write('you ded cuz you idled')
+    conn.write('you ded cuz you idled')//sending message to server
   });
 
-  conn.on("data", (data) => {
+  conn.on("data", (data) => {//the message from server
     console.log(data) //handling incoming data
   })
 
@@ -25,7 +26,17 @@ const connect = function () {
 };
 
 
-
-
 console.log("Connecting ...");
 connect();
+
+const num = 42;
+const str = "hello";
+const func = () => {
+  // myFunction's code
+};
+
+module.exports = {
+  myNumber: num, // stores 42 as myNumber
+  myString: str, // stores "hello" as myString
+  myFunction: func, // stores func (the function) as myFunction
+};
