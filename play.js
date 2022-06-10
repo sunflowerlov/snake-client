14aae80d5724a504dd54d95eddf0018e227afa61
Lisha Liang <lishaliang1999@gmail.com>
2022-06-10 20:01:04 +0000

    server

--- play.js
+++ play.js
@@ -1,27 +1,12 @@
-const net = require("net");
-const connect = require("./client.js")
+console.log('hello');
+const  connect  = require("./client");
+const  setupInput  = require("./input");
 
-// let coonect
-const setupInput = function () {
-  const stdin = process.stdin;//stdin allow us to listen for keyboard input and react
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
 
-  const handleUserInput = function (key) {
-    console.log(key)
+connect();
 
-    // 'q' will stop process
-    if(key === 'q') {
-      process.exit()
-    }
-  };
-  stdin.on("data", handleUserInput);//"data" => user input , representing (key) here
-  return stdin;
-};
+setupInput();
 
-// connect()
-setupInput()
-module.exports = { A: 42, B: 43, C: 44 };
 
-//https://stackoverflow.com/questions/14406108/how-to-trigger-event-stdin-ondata-callback-in-the-code
+
+
