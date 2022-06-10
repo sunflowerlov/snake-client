af01fb50ee450f4eb59e61c3a9db82da283f53f3
Lisha Liang <lishaliang1999@gmail.com>
2022-06-10 21:11:30 +0000

    input action

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
+const net = require("net");
 
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;//stdin allow us to listen for keyboard input and react
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,13 +13,31 @@ const setupInput = function () {
     console.log(key)
 
     // 'q' will stop process
-    if(key === 'q') {
+    if (key === 'q') {
       process.exit()
     }
+
+    if (key === 'w') {
+      connection.write("Move: up")//sending message to server
+    }
+
+    if (key === 'a') {
+      connection.write("Move: left")//sending message to server
+    }
+
+    if (key === 's') {
+      connection.write("Move: down")//sending message to server
+    }
+
+    if (key === 'd') {
+      connection.write("Move: right")//sending message to server
+    }
+
   };
   stdin.on("data", handleUserInput);//"data" => user input , representing (key) here
   return stdin;
 };
 
 // connect()
+
 module.exports = setupInput;
