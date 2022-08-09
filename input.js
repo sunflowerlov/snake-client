a4ca97b11c2983ccd45477557221ce20a8330eac
Lisha Liang <lishaliang1999@gmail.com>
2022-08-09 22:34:16 +0000

    user

--- input.js
+++ input.js
@@ -1,43 +1,33 @@
-const net = require("net");
-
 let connection;
 
 const setupInput = function (conn) {
   connection = conn;
-  const stdin = process.stdin;//stdin allow us to listen for keyboard input and react
+  const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  const handleUserInput = function (key) {
-    console.log(key)
-
-    // 'q' will stop process
-    if (key === 'q') {
-      process.exit()
-    }
-
-    if (key === 'w') {
-      connection.write("Move: up")//sending message to server
-    }
-
-    if (key === 'a') {
-      connection.write("Move: left")//sending message to server
-    }
-
-    if (key === 's') {
-      connection.write("Move: down")//sending message to server
-    }
-
-    if (key === 'd') {
-      connection.write("Move: right")//sending message to server
-    }
-
-  };
-  stdin.on("data", handleUserInput);//"data" => user input , representing (key) here
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
-// connect()
+const handleUserInput = function (key) {
+  // your code here
+  if (key === "\u0003") {
+    process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+};
 
 module.exports = setupInput;
