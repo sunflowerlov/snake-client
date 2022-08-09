f5706f7ed758df46cf9713ea54ac04b1c30fc774
Lisha Liang <lishaliang1999@gmail.com>
2022-08-09 22:35:07 +0000

    client

--- client.js
+++ client.js
@@ -1,48 +1,27 @@
 const net = require("net");
-const { IP, PORT } = require("./constant");
-
 
 const connect = function () {
-  console.log("Connecting ...");
-
   const conn = net.createConnection({
-    host: IP,
-    port: PORT,
+    host: 'localhost',// IP address here,
+    port: 50541// PORT number here,
   });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log('connected to snake server')
-    conn.write("Name: LLS")//sending message to server
-    conn.write("Say: hello")
+    console.log("Successfully connected to game server");
+    conn.write("Name: LLS");
+    conn.write("Say: hello");
+ 
   });
 
+  conn.on("data", (data) => {
+    console.log(data) //you crashed, so you ded.
+  });  
 
-  // conn.on("connect", () => {
-  //   setTimeout(() => {
-  //     conn.write("Move: up")//sending message to server
-  //   }, 1000)
-  // });  
-
-  // conn.on("connect", () => {
-  //   setTimeout(() => {
-  //     conn.write("Move: left")//sending message to server
-  //   }, 1300)
-  // });  
-
-
-  conn.on("data", (data) => {//the message from server
-    console.log(data) //handling incoming data
-  })
-
-  // console.log('you ded cuz you idled')
   return conn;
-
 };
-module.exports = connect
 
-// console.log(myObject); // => { A: 42, B: 43, C: 44 }
-// console.log(myObject.A); // => 42
-// console.log(myObject.B); // => 43
-// console.log(myObject.C); // => 44
+
+module.exports = connect
